f98b955242fb7c18f77d57c8a3f947a6ad406f4c
Qian Lei <qianlei@caicloud.io>
2020-03-21 23:54:53 +0800

    fix(k8): add kubectx completion dir to fpath

--- custom/plugins/spacez_config_k8s/spacez_config_k8s.plugin.zsh
+++ custom/plugins/spacez_config_k8s/spacez_config_k8s.plugin.zsh
@@ -21,5 +21,6 @@ function spacez_load_k8s() {
     if [ -d "$SPACEZ_CONFIG_ZSH_KUBECTX_DIR" ]; then
         alias kns="$SPACEZ_CONFIG_ZSH_KUBECTX_DIR/kubens"
         alias kctx="$SPACEZ_CONFIG_ZSH_KUBECTX_DIR/kubectx"
+        fpath=($fpath "$SPACEZ_CONFIG_ZSH_KUBECTX_DIR/completion")
     fi
 }
