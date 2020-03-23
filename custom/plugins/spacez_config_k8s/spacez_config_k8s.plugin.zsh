9fae86e1cf236861cd15993049fba206596b07c1
Qian Lei <qianlei@caicloud.io>
2020-03-23 10:18:49 +0800

    fix(k8s): remove spacez_load_k8s

--- custom/plugins/spacez_config_k8s/spacez_config_k8s.plugin.zsh
+++ custom/plugins/spacez_config_k8s/spacez_config_k8s.plugin.zsh
@@ -1,26 +1,11 @@
 #!/bin/zsh
 
-function spacez_load_k8s() {
-    if type kubectl > /dev/null 2>&1; then
-        source <(kubectl completion zsh)
-        alias k="kubectl"
-    fi
+if type kubectl > /dev/null 2>&1; then
+    alias k="kubectl"
+fi
 
-    if type helm > /dev/null 2>&1; then
-        source <(helm completion zsh)
-    fi
-
-    if type minikube > /dev/null 2>&1; then
-        source <(minikube completion zsh)
-    fi
-
-    if type istioctl > /dev/null 2>&1; then
-        alias i="istioctl"
-    fi
-
-    if [ -d "$SPACEZ_CONFIG_ZSH_KUBECTX_DIR" ]; then
-        alias kns="$SPACEZ_CONFIG_ZSH_KUBECTX_DIR/kubens"
-        alias kctx="$SPACEZ_CONFIG_ZSH_KUBECTX_DIR/kubectx"
-        fpath=($fpath "$SPACEZ_CONFIG_ZSH_KUBECTX_DIR/completion")
-    fi
-}
+if [ -d "$SPACEZ_CONFIG_ZSH_KUBECTX_DIR" ]; then
+    alias kns="$SPACEZ_CONFIG_ZSH_KUBECTX_DIR/kubens"
+    alias kctx="$SPACEZ_CONFIG_ZSH_KUBECTX_DIR/kubectx"
+    fpath=($fpath "$SPACEZ_CONFIG_ZSH_KUBECTX_DIR/completion")
+fi
