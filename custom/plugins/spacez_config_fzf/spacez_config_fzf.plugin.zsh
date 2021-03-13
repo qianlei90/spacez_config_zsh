1d8b4765785994952ba11cbced392c9863ee6f1e
Qian Lei <qianlei@caicloud.io>
2021-03-13 19:24:31 +0800

    fix(fzf): rename fzf env name

--- custom/plugins/spacez_config_fzf/spacez_config_fzf.plugin.zsh
+++ custom/plugins/spacez_config_fzf/spacez_config_fzf.plugin.zsh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-if [ -d "$SPACEZ_CONFIG_ZSH_FZF_DIR" ]; then
+if [ -d "$SPACEZ_CONFIG_ZSH_FZF_SHELL_DIR" ]; then
     export FZF_DEFAULT_OPTS='--height 40% --reverse --exact'
 
-    source "${SPACEZ_CONFIG_ZSH_FZF_DIR}/shell/completion.zsh"
-    source "${SPACEZ_CONFIG_ZSH_FZF_DIR}/shell/key-bindings.zsh"
+    source "${SPACEZ_CONFIG_ZSH_FZF_SHELL_DIR}/completion.zsh"
+    source "${SPACEZ_CONFIG_ZSH_FZF_SHELL_DIR}/key-bindings.zsh"
 
     alias -g R='| fzf -m'
 fi
