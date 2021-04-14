b3fb767875bb7322e774cefb3465bf3c91a7ffd2
Qian Lei <qianlei90@gmail.com>
2021-04-14 12:29:09 +0800

    fix(zsh): fix k8s

--- custom/plugins/spacez_config_load/k8s.zsh
+++ custom/plugins/spacez_config_load/k8s.zsh
@@ -2,9 +2,14 @@
 
 function load_k8s() {
     source ${SPACEZ_CONFIG_ZSH_OH_MY_ZSH_DIR}/plugins/kubectl/kubectl.plugin.zsh
+    source ${SPACEZ_CONFIG_ZSH_OH_MY_ZSH_DIR}/plugins/helm/helm.plugin.zsh
 
-    if type kubectl > /dev/null 2>&1; then
+    if [ $commands[kubectl] ]; then
         alias k="kubectl"
         alias -g Y="-o yaml"
+    fi
+
+    if [ $commands[vela] ]; then
+        source <(vela completion zsh)
     fi
 }
