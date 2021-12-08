b91ea9d15f33bd5b978f27aadcd5ad4e4491d3ef
Qian Lei <qianlei90@gmail.com>
2021-12-08 11:30:02 +0800

    fix(*): use github

--- custom/proxy.zsh
+++ custom/proxy.zsh
@@ -2,7 +2,7 @@
 
 # 代理
 if [ ! -z "$$SPACEZ_CONFIG_ZSH_PROXY" ]; then
-    alias h="http_proxy=${SPACEZ_CONFIG_ZSH_PROXY} https_proxy=${SPACEZ_CONFIG_ZSH_PROXY} no_proxy=localhost,127.0.0.1,.bugfree.show"
+    alias h="http_proxy=${SPACEZ_CONFIG_ZSH_PROXY} https_proxy=${SPACEZ_CONFIG_ZSH_PROXY} no_proxy=localhost,127.0.0.1,.qianlei.space"
     alias exh="export http_proxy=${SPACEZ_CONFIG_ZSH_PROXY} && export https_proxy=${SPACEZ_CONFIG_ZSH_PROXY}"
     alias unexh="unset http_proxy && unset https_proxy"
 fi
